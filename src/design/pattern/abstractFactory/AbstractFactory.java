ab568454cae12b62b7c10bf5fb73867dbb32c7e4
Dulanji1 <mddulanjicooray@gmail.com>
2022-07-31 20:16:26 +0530

    add all

---
+++ src/design/pattern/abstractFactory/AbstractFactory.java
@@ -0,0 +1,10 @@
+package design.pattern.abstractFactory;
+
+public abstract class AbstractFactory {
+	
+	public abstract Color getColor(String type);
+	
+	public abstract Shape getShape(String type);
+	
+
+}
